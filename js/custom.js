5004774f07fc0273714be88f9571ad974eacbf8a
worldxaker <djxakir714@gmail.com>
2017-04-07 04:34:24 +0300

    update2

--- js/custom.js
+++ js/custom.js
@@ -46,10 +46,109 @@ $(function(){
     jQuery(document).ready(function() {
     $('#home').backstretch([
        //"images/home-bg-slideshow1.jpg",
-       "images/home-bg-slideshow2.jpg",
-        ],  {duration: 2000, fade: 750});
+       "images/IMG_2520.jpg",
+        ],  {duration: 2000, fade: 1000, alignY:top});
     });
   })
 
 });
 
+
+function clicked( stroka){
+    swal.setDefaults({
+        input: 'text',
+        confirmButtonText: 'Далее',
+        cancelButtonText:'Отмена',
+        showCancelButton: true,
+        animation: false,
+        progressSteps: ['1', '2', '3'],
+
+
+    })
+
+    var steps = [
+        {
+            title:'Введите ваше имя'
+        },
+        {
+            title:'Введите ваш email'
+        },
+        {
+            title: 'Опишите проблему',
+            input: 'textarea',
+            confirmButtonText:stroka,
+            showLoaderOnConfirm: true,
+        }
+    ]
+
+    swal.queue(steps).then(function (result) {
+        swal.resetDefaults();
+
+        data={
+            Name:result[0],
+            email:result[1],
+            Problem:result[2]
+        }
+/*
+        $.post( "http://lending-advokat.azurewebsites.net/api/values", function( data ) {
+        },'json').done(function() {
+            alert( "second success" );
+        }).fail(function(e) {
+            alert( "error"+e );
+        });
+        *//*
+        $.ajax({
+            type        :   'POST'  ,
+            url         :   "http://lending-advokat.azurewebsites.net/api/values",
+            data        :   data,
+            contentType :   'application/json'
+        });*/
+
+
+
+/*
+        fetch('http://lending-advokat.azurewebsites.net/api/values', {mode: 'cors'})
+            .then(function(response) {
+                return response.text();
+            })
+            .then(function(text) {
+                console.log('Request successful', text);
+            })
+            .catch(function(error) {
+                log('Request failed', error)
+            });*/
+
+       fetch('http://lending-advokat.azurewebsites.net/api/values/'+JSON.stringify(data), {
+            method: 'GET',
+            mode: 'no-cors',
+            headers: {
+                'Accept': 'application/x-www-form-urlencoded, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            //body: JSON.stringify(data)
+        })
+
+
+
+/*
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:65473/api/values",
+            headers: { 'Accept': 'application/x-www-form-urlencoded' },
+            data:JSON.stringify(data)
+        });*/
+
+
+
+
+
+
+        swal(
+            'Good job!',
+            'You clicked the button!',
+            'success'
+        )
+    }, function () {
+        swal.resetDefaults()
+    })
+}
